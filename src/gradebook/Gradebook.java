4f2a5e0d18ab54faa112a9b8fb87f73c39bbee9e
Leonardo Montes <leonardo.montesm@gmail.com>
2023-04-26 20:59:14 -0500

    add new method to compute the average of the grades for an specific student group

--- src/gradebook/Gradebook.java
+++ src/gradebook/Gradebook.java
@@ -1,14 +1,15 @@
 package gradebook;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class Gradebook {
 
 	/***
 	 * 
 	 * ***/
 	private String courseName;
-
-	
-	
 
 	/**
 	 * @param courseName
@@ -38,6 +39,38 @@ public class Gradebook {
 	public void displayMessage() {
 		System.out.println("Bienvenido a la planilla de notas de la UCaldas " + getCourseName());
 		System.out.println("Imprimir el # de estudiantes");
+	}
+	
+	
+	public void determineClassAverage() {
+		
+		Scanner input = new Scanner (System.in);
+		
+		int total;
+		int gradeCounter;
+		int grade =0;
+		double average=0;
+		
+		total = 0;
+		gradeCounter = 0;
+		
+		while(grade != -1) {
+			
+			total = total + grade;
+			gradeCounter = gradeCounter + 1;
+			
+			System.out.println("Ingrese nueva nota o presion -1 para salir");
+			grade = input.nextInt();
+		}
+		
+		if(gradeCounter != 0) {
+			average = (double) total / gradeCounter;
+		}
+		
+		
+		System.out.println(" La sumatoria de las notas es: " + total);
+		System.out.println(" El promedio de las notas es: " + average);
+		
 	}
 
 }
--- src/gradebook/GradebookTest.java
+++ src/gradebook/GradebookTest.java
@@ -6,15 +6,15 @@ public class GradebookTest {
 	public static void main(String[] args) {
 		
 		Scanner nombreCurso = new Scanner(System.in);
-			
 		
 		
 		Gradebook planillaNotasPoo = new Gradebook("Programacion Orientada a Objetos");
-		Gradebook planillaNotasProgr1 = new Gradebook();
-		Gradebook planillaNotasEstructuras = new Gradebook("Estructuras de datos");
+		//Gradebook planillaNotasProgr1 = new Gradebook();
+		//Gradebook planillaNotasEstructuras = new Gradebook("Estructuras de datos");
 		
 		planillaNotasPoo.displayMessage();
-		planillaNotasEstructuras.displayMessage();
+		
+		planillaNotasPoo.determineClassAverage();
 		
 		
 		
