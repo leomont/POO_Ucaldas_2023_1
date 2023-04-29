868791d2274838ada98d5f9ada008c0bc0b29740
Leonardo Montes <leonardo.montesm@gmail.com>
2023-04-29 15:04:45 -0500

    Add correction for average computing

--- src/gradebook/Gradebook.java
+++ src/gradebook/Gradebook.java
@@ -46,13 +46,15 @@ public class Gradebook {
 		
 		Scanner input = new Scanner (System.in);
 		
-		int total;
-		int gradeCounter;
+		int total =0 ;
+		int gradeCounter= 0;
 		int grade =0;
 		double average=0;
 		
-		total = 0;
-		gradeCounter = 0;
+		
+		System.out.println("Ingrese nueva nota o presion -1 para salir");
+		grade = input.nextInt();
+
 		
 		while(grade != -1) {
 			
@@ -67,7 +69,7 @@ public class Gradebook {
 			average = (double) total / gradeCounter;
 		}
 		
-		
+		System.out.println("Numero de notas" + gradeCounter);
 		System.out.println(" La sumatoria de las notas es: " + total);
 		System.out.println(" El promedio de las notas es: " + average);
 		
