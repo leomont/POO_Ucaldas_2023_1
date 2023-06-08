d7c4927c3444c2afcd8e9074b0dcfe66e2f0bb1c
Leonardo Montes <leonardo.montesm@gmail.com>
2023-06-07 19:22:10 -0500

    Add basic project stub and code demo for polymorphism

---
+++ src/fintech/Cuenta.java
@@ -0,0 +1,47 @@
+package fintech;
+
+public class Cuenta {
+
+
+	private double saldo;
+	private String celular;
+	
+	
+	/**
+	 * @return the saldo
+	 */
+	public double getSaldo() {
+		return saldo;
+	}
+
+
+	/**
+	 * @param saldo the saldo to set
+	 */
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+
+	/**
+	 * @return the celular
+	 */
+	public String getCelular() {
+		return celular;
+	}
+
+
+	/**
+	 * @param celular the celular to set
+	 */
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+
+	public Cuenta() {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/fintech/Usuario.java
@@ -0,0 +1,9 @@
+package fintech;
+
+public class Usuario {
+
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
